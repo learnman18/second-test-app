a38b2a9c091a50de1fd4f97558cb28e821be46e4
Manish Pandey <manishpan18@gmail.com>
2026-09-13 13:43:41 +0530

    added search, sort and order from query params

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,9 +1,9 @@
 const {
-    getUsers: getUsersService,
-    getUserById: getUserByIdService,
-    // createUser: createUserService,
-    updateUser: updateUserService,
-    deleteUser: deleteUserService,
+  getUsers: getUsersService,
+  getUserById: getUserByIdService,
+  // createUser: createUserService,
+  updateUser: updateUserService,
+  deleteUser: deleteUserService,
 } = require('../services/usersService');
 
 //here we are not using next() because we are handling errors in the controller itself and sending appropriate responses to the client.
@@ -11,17 +11,35 @@ const {
 // handling the errors and sending responses directly from the controller functions. const getUsers = async (req, res, next) => {}
 
 const getUsers = async (req, res) => {
-    try {
-        const page = req.query.page ? parseInt(req.query.page) : 1;
-        const limit = req.query.limit ? parseInt(req.query.limit) : 3;
-        if (isNaN(page) || isNaN(limit) || page < 1 || limit < 1 || !Number.isInteger(page) || !Number.isInteger(limit) || limit >= 100) {
-            return res.status(400).json({ message: 'Invalid page or limit parameter' });
-        }
-        const users = await getUsersService({ page, limit });
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({ message: 'Error fetching users', error: error.message });
+  try {
+    //for pagination.
+    const page = req.query.page ? parseInt(req.query.page) : 1;
+    const limit = req.query.limit ? parseInt(req.query.limit) : 3;
+    if (isNaN(page) || isNaN(limit) || page < 1 || limit < 1 || !Number.isInteger(page) || !Number.isInteger(limit) || limit >= 100) {
+      return res.status(400).json({ message: 'Invalid page or limit parameter' });
     }
+    //for sorting and order by - means which field to be sort and in which order, for example user_name in asc order.
+    const sortBy = req.query.sortBy || 'user_id';
+    const order = req.query.order || 'asc';
+    const allowedSortFields = [
+      'user_id',
+      'user_name',
+      'user_email'
+    ];
+    if (!allowedSortFields.includes(sortBy)) {
+      return res.status(400).json({ message: 'Invalid sort field' });
+    }
+    if (order !== 'asc' && order !== 'desc') {
+      return res.status(400).json({ message: 'Invalid order parameter/order' });
+    }
+
+    //search functionality - we can search by user_name and user_email, we can add more fields if required.
+    const search = req.query.search || '';
+    const users = await getUsersService({ page, limit, sortBy, order, search });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching users', error: error.message });
+  }
 };
 
 // const getUsersWithStores = async (req, res) => {
@@ -34,44 +52,44 @@ const getUsers = async (req, res) => {
 // };
 
 const getUserById = async (req, res) => {
-    try {
-        const user = await getUserByIdService(req.params.id);
+  try {
+    const user = await getUserByIdService(req.params.id);
 
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        return res.status(200).json(user);
-    } catch (error) {
-        return res.status(400).json({ message: error.message });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
     }
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
 };
 
 const createUser = async (req, res) => {
-    try {
-        const user = await createUserService(req.body);
-        res.status(201).json({ message: 'User created', user });
-    } catch (error) {
-        res.status(400).json({ message: "user not created", error: error.message });
-    }
+  try {
+    const user = await createUserService(req.body);
+    res.status(201).json({ message: 'User created', user });
+  } catch (error) {
+    res.status(400).json({ message: "user not created", error: error.message });
+  }
 };
 
 const updateUser = async (req, res) => {
-    try {
-        const user = await updateUserService(req.params.id, req.body);
-        res.status(200).json({ message: 'User updated', user });
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  try {
+    const user = await updateUserService(req.params.id, req.body);
+    res.status(200).json({ message: 'User updated', user });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 const deleteUser = async (req, res) => {
-    try {
-        const user = await deleteUserService(req.params.id);
-        res.status(200).json({ message: 'User deleted', user });
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  try {
+    const user = await deleteUserService(req.params.id);
+    res.status(200).json({ message: 'User deleted', user });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 module.exports = { getUsers, getUserById, createUser, updateUser, deleteUser };
--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,23 +1,29 @@
 const prisma = require('../../config/prisma');
 
 //page and limit is query params for pagination, we will use them to get the users in pages, currently hardcoded from controller file, we can check.
-const getUsers = async ({ page, limit }) => {
-    //consider limit is 5 records on one page, limits can differ based on our requirement - users?page=1 as limit is hardcoded to 5, then it will display
-    //first 5 records on page 1 and at that skip is 0, once we make the change users?page=2, then it will display next 5 records on page 2 and at that 
-    //time it will skip previous 5, and similary it will do for other page and for that we have this formula.
-    //skip = (page -1) * limit which is if we are on page 3, skiap = (3-1) * 5 = 10, if we are on page 4, skip = (4-1) * 5 = 15, and it will skip those records
-    //to display
-    const skip = (page - 1) * limit;
+const getUsers = async ({ page, limit, sortBy, order, search }) => {
+  //consider limit is 5 records on one page, limits can differ based on our requirement - users?page=1 as limit is hardcoded to 5, then it will display
+  //first 5 records on page 1 and at that skip is 0, once we make the change users?page=2, then it will display next 5 records on page 2 and at that 
+  //time it will skip previous 5, and similary it will do for other page and for that we have this formula.
+  //skip = (page -1) * limit which is if we are on page 3, skiap = (3-1) * 5 = 10, if we are on page 4, skip = (4-1) * 5 = 15, and it will skip those records
+  //to display
+  const skip = (page - 1) * limit;
   return prisma.user.findMany({
-    orderBy: { user_id: 'asc' },
+    orderBy: { [sortBy]: order }, //sorting and order by.
     include: {
-      stores: true,
-        _count: {
-          select: { stores: true }
-        }
+      stores: true, // return store details.
+      _count: { //return count of total store.
+        select: { stores: true }
+      }
     },
-    skip: skip,
-    take: limit
+    where: { //search functionality - we can search by user_name, we can add more fields if required.
+      user_name: {
+        contains: search,
+        mode: 'insensitive'
+      }
+    },
+    skip: skip, //use for pagiantion
+    take: limit //use for pagiantion
   });
 };
 
@@ -44,9 +50,9 @@ const getUserById = async (id) => {
     where: { user_id: userId },
     include: {
       stores: true,
-        _count: {
-          select: { stores: true }
-        }
+      _count: {
+        select: { stores: true }
+      }
     },
   });
 };
